6c627be3aedb14c32f1f2a2a2d13b31afd1ca31c
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2022-11-24 14:54:12 +0700

    add layout artikel and detail artikel

---
+++ FrontEnd/frontEnd.md
@@ -0,0 +1,2 @@
+Dijalankan menggunakan live server.
+Klik kanan pada file yang akan dibuka, klik open with live servers
---
+++ FrontEnd/src/script/artikel-data.js
@@ -0,0 +1,212 @@
+const artikelData = [
+  {
+    judul: "Mengenal Golden Period dalam Penanganan Stroke",
+    penulis: "dr. Rizal Fadli ",
+    tanggal_terbit: "09 September 2022",
+    img_url:
+      "https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2022/09/09061925/Mengenal-Golden-Period-dalam-penanganan-stroke.jpg.webp",
+    deskripsi: `Stroke adalah salah satu penyebab utama kematian dan kecacatan seumur hidup. Stroke mungkin meninggalkan dampak jangka panjang pada kesehatan dan kehidupan. Cara paling baik untuk membantu meminimalkan risiko terjadinya cacat jangka panjang atau kematian adalah memberikan pengobatan dalam golden hour atau golden period. 
+
+    Sayangnya, kebanyakan orang tidak tahu tentang hal ini. Inilah mengapa tak sedikit pengidap stroke yang berujung mengalami cacat seumur hidup atau bahkan kehilangan nyawa. Sebenarnya, apa itu golden hour atau golden period dalam penanganan stroke?
+    
+    Mengenal Golden Period dalam Penanganan Stroke
+    Stroke terjadi ketika pembuluh darah di otak pecah atau tersumbat. Akibatnya, suplai darah terhambat dan otak tidak mendapatkan oksigen dan nutrisi penting untuk berfungsi secara normal. Ibarat mesin pada tubuh, kelainan fungsi otak bisa berdampak buruk pada kesehatan dan membahayakan nyawa. 
+    
+    Kondisi ini dapat memengaruhi pendengaran, berbicara, makan, bergerak, dan hampir semua proses vital. Inilah sebabnya mengapa perawatan stroke yang tepat selama golden hour sangat krusial bagi nyawa pengidap.
+    
+    Golden hour atau golden period adalah istilah yang digunakan untuk jangka waktu kehidupan pengidap stroke melalui pengobatan dengan segera. Apabila pengidap stroke mendapatkan bantuan medis dalam periode tersebut, besar kemungkinan ia dapat bertahan dari komplikasi stroke.
+    
+    Pada penyakit stroke, periode golden hour adalah empat setengah jam setelah seseorang mengalami gejala stroke. Tanpa adanya penanganan pada rentang waktu tersebut, pengidap stroke sangat mungkin mengalami kerusakan otak permanen.
+    
+    Ketahui Gejala Stroke
+    Untuk lebih memahami gejala dan tindakan pencegahan, pertama-tama perlu diketahui bahwa stroke terbagi menjadi dua jenis, yaitu iskemik dan hemoragik.
+    
+    Stroke iskemik disebabkan oleh penyumbatan atau penyempitan pembuluh darah yang berujung pada penyumbatan aliran darah. Penyumbatan ini dapat berkembang karena pembentukan gumpalan atau timbunan lemak yang menumpuk di pembuluh darah.
+    
+    Sementara itu, stroke hemoragik adalah jenis stroke yang terjadi karena pecah atau bocornya pembuluh darah otak. Kondisi ini disebabkan oleh berbagai hal seperti trauma, penggunaan obat pengencer darah yang berlebihan, stroke iskemik, tekanan darah tinggi yang tidak terkendali, dan faktor lainnya.
+    
+    Supaya lebih mudah mendapatkan diagnosis untuk stroke, dokter menentukan metode untuk mengidentifikasi kelainan ini yang dikenal dengan metode FAST, yaitu:
+    
+    F: Facial dropping atau wajah yang terkulai.
+    A: Arm weakness atau kelemahan pada tangan.
+    S: Speech difficulties atau kesulitan berbicara.
+    T: Time yang menunjukkan kedatangan pengidap ke rumah sakit tepat waktu atau maksimal 4,5 jam setelah gejala muncul. 
+    Setelah stroke diidentifikasi, langkah terpenting adalah segera melakukan pemindaian otak dengan bantuan CT scan. Jika pemeriksaan tersebut tidak menunjukkan adanya perdarahan, obat penghancur bekuan darah dapat diberikan untuk melarutkan bekuan darah. 
+    
+    Bagaimana Stroke Dicegah?
+    Sebagian besar stroke terjadi karena riwayat kesehatan keluarga. Meski demikian, bukan berarti masalah kesehatan ini tidak dapat dicegah. Berikut beberapa caranya:
+    
+    Hindari merokok dan mengonsumsi minuman beralkohol.
+    Tetap aktif secara fisik dengan rutin berolahraga.
+    Faktor risiko seperti kolesterol tinggi, obesitas, hipertensi dan diabetes harus diidentifikasi dan diobati pada tahap awal karena ini bisa menjadi tanda awal stroke.
+    Gaya hidup sehat membantu menjaga tubuh tetap bugar dan sehat. Ini termasuk menerapkan pola makan sehat, tidur yang cukup dan mengendalikan stres dengan baik.`,
+    sumber:
+      "https://www.halodoc.com/artikel/mengenal-golden-period-dalam-penanganan-stroke",
+  },
+  {
+    judul: "Masih Muda, Bisa Juga Kena Stroke",
+    penulis: "dr. Rizal Fadli",
+    tanggal_terbit: "22 Agustus 2022",
+    img_url:
+      "https://d1bpj0tv6vfxyp.cloudfront.net/articles/560524_22-8-2022_12-39-10.webp",
+    deskripsi: `Meskipun kamu masih berusia muda, jangan sepelekan penyakit-penyakit yang biasanya dialami orang yang berusia lanjut. Misalnya penyakit stroke, ternyata dapat menyerang seseorang yang masih berusia muda. Pada 2010, sebuah studi yang dipublikasikan pada jurnal Stroke, menemukan antara 1988 hingga 2004, serangan otak tiga kali lipat terjadi pada perempuan berusia 35 hingga 54 tahun.
+
+    Bahkan pada pertengahan tahun 1990 hingga awal 2000, penelitian yang dipublikasikan dalam Neurology menunjukkan adanya peningkatan stroke sekitar 54 persen terhadap orang dewasa yang berusia 20 hingga 45 tahun. Kamu mungkin berpikir bahwa stroke tidak akan menyerang seseorang yang berusia muda. Mitos tersebut kini terbantahkan.
+    
+    Perlu diketahui bahwa jumlah orang yang terkena stroke pada usia muda justru meningkat. Penelitian yang dipublikasikan dalam jurnal Neurology menunjukkan bahwa pada 1999 dan 2005 di Cincinnati, terjadi penurunan stroke pada orang yang berusia 71 hingga 69 tahun. Namun, ada peningkatan 13 hingga 19 persen pada orang yang berusia 20 hingga 54 tahun. Meskipun hal ini dibantah oleh Andrew Russman, seorang ahli saraf dan spesialis perawatan stroke di Cleveland Clinic.
+    
+    Andrew mengatakan bahwa beberapa penelitian menunjukkan adanya peningkatan stroke pada usia muda, tetapi buktinya kurang. Insiden stroke mengalami penurunan secara keseluruhan, mungkin ini disebabkan adanya edukasi untuk lebih mengenali stroke pada usia muda.
+    
+    Penyebab Stroke Terjadi pada Usia Muda
+    Menurut S. Auzim Azizi, MD, pimpinan jurusan Neurology dan dosen Neurology Temple University Medical School di Philadelphia, “Dibandingkan Stroke pada usia lanjut, stroke di usia muda adalah penyakit yang berbeda.” Infeksi, trauma gangguan jantung, dehidrasi, dan sickle cell disease bisa menjadi penyebab paling umum stroke pada usia muda.
+    
+    Turunnya asupan atau supply darah ke otak mengakibatkan terjadinya stroke. Stroke isemik biasanya menjadi penyebab yang sering terjadi, yaitu karena adanya pembekuan darah di dalam jantung atau pembuluh darah. Penyebab lainnya adalah pembedahan pembuluh darah di leher, yaitu pembekukan yang diakibatkan oleh robekan kecil pada pembuluh darah besar dan darah dikirim ke otak.
+    
+    Migrain, pil KB, kehamilan dan merokok juga diidentifikasi sebagai penyebab stroke usia muda. Menurut peneliti asal Prancis, perubahan hormon yang terjadi pada usia muda, terutama hormon yang membuat seseorang tinggi, dapat meningkatkan risiko dua hingga lima kali.
+    
+    The Collaborative Group for the Study of Stroke in Young Women mengemukakan bahwa pil KB dapat meningkatkan risiko jika diminum oleh perempuan yang memiliki darah tinggi atau migrain, apalagi jika perempuan tersebut perokok berat. Ini disebabkan karena pil kontrasepsi mengubah agregasi platelet, sehingga meningkatkan aktivitas antithrombin III, serta mengakibatkan pembekuan pada tingkat tertentu. Kehamilan juga mampu meningkatkan sekitar 13 kali risiko terjadinya stroke iskemik pada perempuan.
+    
+    Kardiogenik juga bisa menjadi pemicu, yang termasuk dalam kardiogenik adalah sakit jantung, kelainan katup jantung, dan patent foramen ovale (ini merupakan lubang pada jantung di sisi kanan dan kiri). Bahkan, obesitas dan kecanduan alkohol dapat menyebabkan masalah pada jantung sehingga membuat kamu terkena stroke. Narkoba tipe amfetamin, termasuk kokain, meth, dan marijuana adalah hal yang perlu dihindari.
+    
+    Mengenal Gejala Stroke
+    Berikut gejala stroke pada orang dengan usia muda. Jika kamu memiliki gejala berikut, sebaiknya segera mendapatkan perawatan medis:
+    
+    Nyeri kepala berat.
+    Perubahan penglihatan.
+    Lemas.
+    Kebingungan.
+    Kesulitan berbicara.
+    Kesulitan dalam memahami
+    Tingkah laku yang tidak biasa.
+    Penurunan kewaspadaan.
+    Kesulitan berjalan.
+    Buruknya keseimbangan.
+    Stroke di usia muda dapat mengubah hidup. Pelajari lebih lanjut mengenai bagaimana mendapatkan pertolongan dan dukungan. Rehabilitasi pasca stroke bisa membantu mencapai hasil terbaik yang dapat mengarah pada kehidupan bahagia, sehat, dan produktif.
+    
+    Stroke di usia muda dapat dicegah dengan cara olahraga secara teratur, diet sehat, memantau tekanan darah, dan menghentikan gaya hidup tidak sehat (merokok, narkoba, dan alkohol).`,
+    sumber: "https://www.halodoc.com/artikel/masih-muda-bisa-juga-kena-stroke",
+  },
+  {
+    judul: "Inilah Perbedaan antara Stroke Hemoragik dan Stroke Iskemik",
+    penulis: "dr. Rizal Fadli",
+    tanggal_terbit: "22 Agustus 2022",
+    img_url:
+      "https://d1bpj0tv6vfxyp.cloudfront.net/articles/2558_22-8-2022_12-4-43.webp",
+    deskripsi: `Kurang berolahraga, gemar mengonsumsi alkohol, dan kebiasaan merokok adalah gaya hidup yang rentan terhadap beberapa penyakit kronis, salah satunya adalah stroke. Stroke adalah gangguan kesehatan yang disebabkan terganggunya pasokan darah menuju otak akibat penyumbatan atau pecahnya pembuluh darah. Sebaiknya lakukan gaya hidup yang lebih sehat untuk menghindari penyakit stroke. 
+
+    Ketahui Penyebab Penyakit Stroke
+    Tanpa asupan darah yang cukup menuju otak tentu sel-sel dalam otak mati. Kondisi ini yang dapat menyebabkan komplikasi pada pengidap stroke. Ada dua penyebab seseorang mengalami penyakit stroke, pertama karena penyumbatan pada saluran darah menuju otak yang dikenal juga dengan stroke iskemik. Kedua, disebabkan pecahnya pembuluh darah atau dikenal dengan stroke hemoragik.
+    
+    Lalu, bagaimana cara membedakan kedua penyebab stroke ini? Sebelum membedakan kedua penyebab stroke, sebaiknya ketahui lebih banyak tentang stroke hemoragik dan stroke iskemik.
+    
+    1. Stroke Hemoragik
+    Seseorang dikatakan mengalami stroke hemoragik ketika pembuluh darah pada otak mengalami pecah atau kebocoran. Awalnya, melemahnya pembuluh darah seseorang membuat pembuluh darah pada otak pecah. Darah yang tumpah akibat pecah atau bocor menyebabkan penumpukan dan menghambat jaringan otak. Jika tidak segera diatasi, kondisi ini dapat menyebabkan kematian atau keadaan koma yang panjang.
+    
+    2. Stroke Iskemik
+    Stroke iskemik terjadi karena ada penyumbatan oleh darah beku pada aliran darah menuju otak. Pembekuan darah pada aliran darah menuju otak disebabkan oleh timbunan lemak pada aliran darah. 
+    
+    Ketahui Letak Perbedaan Kedua Jenis Stroke Ini
+    Gejala stroke yang umum terjadi adalah kondisi tungkai yang mati rasa, sulit berbicara sehingga menyebabkan bicara menjadi kacau dan biasanya pengidap stroke mengalami kondisi wajah yang lebih menurun. Tidak hanya itu, gejala lain dapat terjadi dan disesuaikan dengan jenis stroke yang dialami pengidapnya.
+    
+    Stroke hemoragik menimbulkan gejala seperti kejang-kejang pada pengidapnya. Perawatan pada stroke hemoragik juga lebih menekankan pengendalian perdarahan yang terjadi. Kondisi stroke hemoragik dapat meningkatkan risiko kematian yang cukup besar pada pengidapnya.
+    
+    Sedangkan pengobatan untuk stroke iskemik dilakukan dengan menangani dan menghilangkan gumpalan darah yang terjadi akibat pembekuan darah. Penggunaan obat seperti aspirin bisa dilakukan dalam keadaan darurat. Pemberian aspirin juga dapat dilakukan setelah memastikan bahwa pasien mengidap stroke iskemik.
+    
+    Pencegahan Penyakit Stroke
+    Gaya hidup sehat dapat menghindari kamu dari penyakit stroke. Hindari berat badan yang berlebihan atau obesitas karena meningkatkan risiko stroke. Selain itu, jangan lupa untuk tetap rutin melakukan olahraga. Tidak hanya mencegah penyakit stroke, rutin berolahraga juga dapat menghindari kamu dari berbagai penyakit salah satunya gangguan jantung.
+    
+    Pola makan sehari-hari juga perlu diperhatikan dengan baik. Terlalu banyak mengonsumsi makanan yang mengandung garam tinggi mengganggu kesehatan pembuluh darah. Selain itu, pilih makanan yang memiliki lemak sehat. Mengonsumsi lemak jenuh menyebabkan kolesterol naik yang menyebabkan gumpalan pada aliran darah.`,
+    sumber:
+      "https://www.halodoc.com/artikel/inilah-perbedaan-antara-stroke-hemoragik-dan-stroke-iskemik",
+  },
+  {
+    judul: "Jangan Main-Main dengan Stroke Ringan, Ini 4 Cara Mengatasinya",
+    penulis: "dr. Fadhli Rizal Makarim",
+    tanggal_terbit: "22 Agustus 2022",
+    img_url:
+      "https://d1bpj0tv6vfxyp.cloudfront.net/articles/460219_18-8-2022_11-17-17.webp",
+    deskripsi: `Penyakit ini dikenal sebagai  the silent killer, lantaran penyakit ini sangat berbahaya dan bisa membunuh secara diam-diam akibat kelumpuhan otak. Kalau tak menyebabkan kematian, stroke di usia bisa membawa dampak kecacatan bagi pengidapnya. Mengerikan, bukan?
+
+    Sebagian dari kita mungkin tak asing dengan stroke, tetapi bagaimana dengan transient ischaemic attack (TIA) atau stroke ringan? Meski menyandang kata “ringan”, TIA tau stroke ringan tak boleh diabaikan. Sebab, bisa menimbulkan dampak serius di kemudian hari. Pertanyaannya, bagaimana sih cara mengatasi stroke ringan?
+    
+    Awasi Gejala yang Menyerang Tiba-Tiba
+    Sebelum mengetahui cara mengatasi stroke ringan, tak ada ada salahnya untuk berkenalan dulu dengan gejalanya. Gejala stroke ringan kebanyakan terjadi secara tiba-tiba. Boleh dibilang gejala stroke ringan atau TIA hampir serupa dengan stroke.
+    
+    Bedanya gejala stroke ringan hanya berlangsung beberapa menit dan akan hilang dengan sendirinya dalam hitungan jam. Lalu, seperti apa sih gejala stroke ringan yang umumnya dialami oleh pengidapnya?
+    
+    Nah, berikut beberapa gejalanya menurut ahli di National Institutes of Health - MedlinePlus.
+    
+    Perubahan pada indra, seperti pendengaran, penglihatan, rasa, dan sentuhan.
+    Perubahan kewaspadaan (termasuk kantuk atau tidak sadar).
+    Perubahan mental, seperti kebingungan, kehilangan ingatan, kesulitan menulis atau membaca, kesulitan berbicara, atau memahami orang lain.
+    Masalah otot, contohnya kelemahan otot, kesulitan menelan, atau kesulitan berjalan.
+    Pusing atau kehilangan keseimbangan dan koordinasi.
+    Kurangnya kontrol atas kandung kemih atau usus.
+    Masalah saraf, seperti mati rasa atau kesemutan pada satu sisi
+    Umumnya sekitar 70 persen gejala stroke ringan bisa hilang kurang dari 10 menit, atau 90 persen akan hilang kurang dari empat jam. Ingat, segeralah temui atau tanyakan pada dokter bilang mengalami gejala stroke ringan di atas. Kamu bisa kok bertanya langsung pada dokter melalui aplikasi Halodoc.
+    
+    Kembali ke tajuk utama, bagaimana sih cara mengatasi stroke ringan?
+    
+    Perubahan Gaya Hidup sampai Operasi
+    Cara untuk mengatasi stroke ringan beragam. Penanganannya pada pengidapnya berebeda-beda, bergantun pada usia, penyebab stroke, dan kondisi medis pengidapnya. Penanganan stroke ringan ini bertujuan untuk mengobati gangguan yang memicu stroke ringan dan mencegah risiko terjadinya stroke yang lebih parah.
+    
+    Lalu, seperti apa metode atau cara mengatasi stroke ringan?
+    
+    Perubahan Gaya Hidup
+    Menurut ahli di National Institutes of Health, pengidap stroke ringan akan didorong untuk melakukan perubahan gaya hidup. Tujuannya jelas, untuk mengurangi risiko berkembangnya gejala TIA. Perubahan gaya hidup ini mencakup berhenti merokok, rutin berolahraga, dan megonsumsi makanan sehat atau bergizi seimbang.
+    
+    Konsumsi Obat-obatan
+    Konsumsi atau terapi obat-obatan bertujuan untuk mengurangi risiko stroke akibat TIA. Obat-obatan yang diberikan seperti obat pengencer darah, misalnya aspirin atau Coumadin, untuk mengurangi pembekuan darah. Di samping itu, ada pula obat antihipertensi, obat statin, atau obat antikaogulan yang mungkin diberikan oleh dokter.
+    
+    Menyingkirkan Infeksi
+    Menurut ahli di American Stroke Association, beberapa penyebab TIA hanya bisa terlihat lewat pemeriksaan atau peralatan khusus di rumah sakit. Ketika TIA terjadi pada orang muda tanpa faktor risiko yang jelas, mereka mungkin dikirim ke ahli saraf untuk menelisik kondisinya lebih jauh.
+    
+    Ahli saraf nantinya akan melakukan tindakan ketika penyebabnya sudah diketahui. Misalnya, menyingkirkan vasculitis (peradangan pada pembuluh darah), diseksi arteri karotis, atau infeksi lainnya.
+    
+    Operasi
+    Cara mengatasi stroke ringan juga bisa melalui operasi. Operasi ini biasanya dilakukan pada mereka yang mengalami penyumbatan arteri leher. Prosedur ini disebut dengan endarterektomi.`,
+    sumber:
+      "https://www.halodoc.com/artikel/jangan-main-main-dengan-stroke-ringan-ini-4-cara-mengatasinya",
+  },
+  {
+    judul: "",
+    penulis: "",
+    tanggal_terbit: "",
+    img_url: "",
+    deskripsi: ``,
+    sumber: "",
+  },
+  {
+    judul: "",
+    penulis: "",
+    tanggal_terbit: "",
+    img_url: "",
+    deskripsi: ``,
+    sumber: "",
+  },
+  {
+    judul: "",
+    penulis: "",
+    tanggal_terbit: "",
+    img_url: "",
+    deskripsi: ``,
+    sumber: "",
+  },
+  {
+    judul: "",
+    penulis: "",
+    tanggal_terbit: "",
+    img_url: "",
+    deskripsi: ``,
+    sumber: "",
+  },
+  {
+    judul: "",
+    penulis: "",
+    tanggal_terbit: "",
+    img_url: "",
+    deskripsi: ``,
+    sumber: "",
+  },
+];
+export default artikelData;
