85d1ba3a5a815713f6c41b5fbf45e4d67f32aca6
syiidahasnaa30 <syiidahasnaa30@gmail.com>
2022-12-07 22:38:41 +0700

    fix footer

--- FrontEnd/src/script/component/footer-healthcare.js
+++ FrontEnd/src/script/component/footer-healthcare.js
@@ -4,13 +4,32 @@ class FooterHealthcare extends HTMLElement {
   }
   render() {
     this.innerHTML = `
+    <div class="inner-footer">
     <span class="logo-white">Healthcare</span>
     <div class="d-md-flex flex-md-row text-white">
       <div class="sub-section">
         <p class="pb-3 pt-2">Jl. XXX, Indonesia</p>
-        <p class="pb-2"><a class="text-white"href="./about-page.html">Tentang Kami</a></p>
-        <p class="pb-2"><a class="text-white"href="./privacy-polici.html">Kebijakan Privasi</a></p>
       </div>
+      <div class="sub-section">
+        <p class="pb-2 pt-2" style="font-weight: 700">NAVIGASI</p>
+        <p class="pb-2"><a class="text-white"href="./index.html">Beranda</a></p>
+        <p class="pb-2"><a class="text-white"href="./about-page.html">Tentang Kami</a></p>
+        <p class="pb-2"><a class="text-white"href="./input-page.html">Cek Kesehatan</a></p>
+        <p class="pb-2"><a class="text-white"href="./artikel-page.html">Artikel Kesehatan</a></p>
+      </div>
+      <div class="sub-section">
+        <p class="pb-2 pt-2" style="font-weight: 700">KONTAK KAMI</p>
+        <p class="pb-2"><a class="text-white"href="./index.html">Nikolas Edo</a></p>
+        <p class="pb-2"><a class="text-white"href="./about-page.html">Tentang Kami</a></p>
+        <p class="pb-2"><a class="text-white"href="./input-page.html">Cek Kesehatan</a></p>
+        <p class="pb-2"><a class="text-white"href="./artikel-page.html">Artikel Kesehatan</a></p>
+      </div>
+      <div class="sub-section">
+      <p class="pb-3 pt-2">Jl. XXX, Indonesia</p>
+      <p class="pb-2"><a class="text-white"href="./about-page.html">Tentang Kami</a></p>
+      <p class="pb-2"><a class="text-white"href="./privacy-polici.html">Kebijakan Privasi</a></p>
+    </div>
+
 
       <div class="sub-section">
         <p class="pb-2 pt-2" style="font-weight: 700">Hubungi Kami</p>
@@ -22,7 +41,10 @@ class FooterHealthcare extends HTMLElement {
         </p>
       </div>
     </div>
-    <p class="text-white text-center pt-3">Created by C22-083 team, &#169; 2022</p>
+    </div>
+    
+    <hr>
+    <p class="text-white text-center pt-3">Copyright &#169; 2022 - Healthcare. All Right Reserved</p>
       `;
   }
 }
