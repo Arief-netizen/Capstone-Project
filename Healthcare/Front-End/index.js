9ab2dd439c6777bc7db1b2fb55d275ea4470d893
Ariefnetizen <56224972+Arief-netizen@users.noreply.github.com>
2022-12-09 22:15:57 +0700

    Add files via upload

---
+++ Healthcare/Front-End/index.js
@@ -0,0 +1,6 @@
+// import "./script/component/footer-healthcare.js";
+// import "./script/component/app-bar.js";
+// import "./script/component/beranda/beranda.js";
+// import "./script/component/input-page.js";
+// import "./script/component/artikel/artikel-page.js";
+// import "./script/component/artikel/artikel-detail.js";
