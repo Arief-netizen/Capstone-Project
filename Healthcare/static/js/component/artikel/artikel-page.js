c158532ac4db0e8ddac4af4219d632756fcff16a
Ariefnetizen <56224972+Arief-netizen@users.noreply.github.com>
2022-12-09 22:27:18 +0700

    Add files via upload

---
+++ Healthcare/static/js/component/artikel/artikel-page.js
@@ -0,0 +1,16 @@
+// import "./artikel-list.js";
+// import "./artikel-searchbar.js";
+// class ArtikelPage extends HTMLElement {
+//   connectedCallback() {
+//     this.render();
+//   }
+//   render() {
+//     this.innerHTML = `
+//     <div class="section">
+//       <artikel-searchbar></artikel-searchbar>
+//       <artikel-list class="row artikel-list mt-5 mb-5"></artikel-list>
+//     </div>   
+//       `;
+//   }
+// }
+// customElements.define("artikel-page", ArtikelPage);
